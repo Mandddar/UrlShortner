303341b806dbd7b11e9b992f14f620d4666588c3
Mandddar <mandddar04@gmail.com>
2026-07-25 16:21:48 +0530

    crud is done

--- app/routers/url.py
+++ app/routers/url.py
@@ -1,13 +1,22 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
- 
+
 from app.dependencies.database import get_db
 from app.schemas.url import URLCreate, URLResponse
-from app.services.url_services import create_url, get_all_urls as get_all_urls_service
-router =APIRouter(
-    prefix="/url",
-    tags=["url"]
+from app.services.url_services import (
+    create_url,
+    get_all_urls,
+    get_url_by_id,
+    update_url,
+    delete_url,
 )
+
+router = APIRouter(
+    prefix="/url",
+    tags=["URL"]
+)
+
+# CREATE
 @router.post("/", response_model=URLResponse)
 def create_short_url(
     url: URLCreate,
@@ -15,14 +24,38 @@ def create_short_url(
 ):
     return create_url(db, url)
 
+
+# READ ALL
 @router.get("/", response_model=list[URLResponse])
-def list_urls(db: Session = Depends(get_db)):
-    urls = get_all_urls_service(db)
+def read_urls(
+    db: Session = Depends(get_db)
+):
+    return get_all_urls(db)
 
-    for u in urls:
-        print("ID:", u.id)
-        print("URL:", u.original_url)
-        print("SHORT:", u.short_code)
-        print("----------------")
 
-    return urls
+# READ ONE
+@router.get("/{url_id}", response_model=URLResponse)
+def read_url(
+    url_id: int,
+    db: Session = Depends(get_db)
+):
+    return get_url_by_id(db, url_id)
+
+
+# UPDATE
+@router.put("/{url_id}", response_model=URLResponse)
+def edit_url(
+    url_id: int,
+    url: URLCreate,
+    db: Session = Depends(get_db)
+):
+    return update_url(db, url_id, url)
+
+
+# DELETE
+@router.delete("/{url_id}")
+def remove_url(
+    url_id: int,
+    db: Session = Depends(get_db)
+):
+    return delete_url(db, url_id)
--- app/services/url_services.py
+++ app/services/url_services.py
@@ -3,6 +3,8 @@ from app.models.url import Url
 from app.schemas.url import URLCreate
 import random
 import string
+from app.schemas.url import URLCreate
+from fastapi import HTTPException
 def generate_short_code(length=6):
     return "".join(
         random.choices(
@@ -23,3 +25,37 @@ def create_url(db: Session, url_create: URLCreate):
     return db_url
 def get_all_urls(db: Session):
     return db.query(Url).all()
+def get_url_by_id(db: Session, url_id: int):
+    return db.query(Url).filter(Url.id == url_id).first()
+
+def update_url(db: Session, url_id: int, url_data: URLCreate):
+    url = db.query(Url).filter(Url.id == url_id).first()
+
+    if url is None:
+        raise HTTPException(
+            status_code=404,
+            detail="URL not found"
+        )
+
+    url.original_url = str(url_data.original_url)
+
+    db.commit()
+    db.refresh(url)
+
+    return url
+
+def delete_url(db: Session, url_id: int):
+    url = db.query(Url).filter(Url.id == url_id).first()
+
+    if url is None:
+        raise HTTPException(
+            status_code=404,
+            detail="URL not found"
+        )
+
+    db.delete(url)
+    db.commit()
+
+    return {
+        "message": "URL deleted successfully"
+    }
